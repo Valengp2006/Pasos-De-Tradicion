ddeba5567a3a76427b53252946e067b618437699
Valengp2006 <valentinagp2006@gmail.com>
2026-05-20 17:26:55 -0500

    corregir errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://valengp2006.github.io',
   base: '/pasos-de-tradicion/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
