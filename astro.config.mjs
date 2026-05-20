bf5f26893cd03895a2ac264d9127ef08abc9f074
Valengp2006 <valentinagp2006@gmail.com>
2026-05-20 16:48:12 -0500

    Configurar la ruta base para publicación

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://TU-USUARIO-DE-GITHUB.github.io', // <- Cambia esto por tu usuario de GitHub
-  base: '/pasos-de-tradicion',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https:Valengp2006.github.io',
+  base: '/pasos-de-tradicion/', 
 });
