6d8d3fb2bcc4d86a72274e9acd4657c2a1c90200
Valengp2006 <valentinagp2006@gmail.com>
2026-05-20 17:02:53 -0500

    Agregar deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https:Valengp2006.github.io',
-  base: '/pasos-de-tradicion/', 
+  site: 'https://valengp2006.github.io',
+  base: '/pasos-de-tradicion/',
 });
